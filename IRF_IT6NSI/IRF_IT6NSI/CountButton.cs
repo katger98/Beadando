72e6102f1255f4e57b70a96be1b97efb6c5a82cf
katger98@gmail.com <katger98@gmail.com>
2020-12-13 19:00:21 +0100

    A saját gomb osztályunk kialakitása, a saját gombjaink illetve képek elhelyezése a formon

---
+++ IRF_IT6NSI/IRF_IT6NSI/CountButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_IT6NSI
+{
+    public class CountButton : Button
+    {
+        private int privvalue;
+        public int Value
+        {
+            get { return privvalue; }
+            set
+            {
+                privvalue = value;
+                if (privvalue>=5)
+                {
+                    Text =privvalue.ToString() + " !";
+                }
+                else
+                {
+                    Text = privvalue.ToString();
+                }
+            }
+        }
+        public CountButton()
+        {
+            Height = 50;
+            Width = Height;
+            BackColor = Color.White;
+            Value = 0;
+            Enabled = false;
+        }
+    }
+}
--- IRF_IT6NSI/IRF_IT6NSI/Form1.cs
+++ IRF_IT6NSI/IRF_IT6NSI/Form1.cs
@@ -15,6 +15,25 @@ namespace IRF_IT6NSI
         public Form1()
         {
             InitializeComponent();
+            DrawFormElements();
+        }
+       private void DrawFormElements()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                CountButton cb = new CountButton();
+                cb.Top=Convert.ToInt32(this.Height*0.85);
+                cb.Left = Convert.ToInt32(this.Width * 0.1) + i * Convert.ToInt32(this.Width * 0.25);
+                Controls.Add(cb);
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Image =Image.FromFile( "images/"+(i+1).ToString() + ".jpg");
+                pictureBox.Height = 100;
+                pictureBox.Width = pictureBox.Height;
+                pictureBox.Top= Convert.ToInt32(this.Height * 0.67);
+                pictureBox.Left = Convert.ToInt32(this.Width * 0.075) + i * Convert.ToInt32(this.Width * 0.25);
+                pictureBox.SizeMode =PictureBoxSizeMode.StretchImage;
+                Controls.Add(pictureBox);
+            }
         }
     }
 }
